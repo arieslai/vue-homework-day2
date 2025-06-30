eabc1f622f8357824eaa6c5b6991fb523430ff63
arieslai <aries120343@gmail.com>
2025-06-30 14:40:00 +0800

    feat: 新增網址收藏功能

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,72 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+
+const newTitle = ref('')
+const newUrl = ref('')
+const urlCollections = ref([]);
+
+onMounted(() => {
+  const storedUrlCollections = localStorage.getItem('auo-url-collection-tool');
+  if (storedUrlCollections) {
+    urlCollections.value = JSON.parse(storedUrlCollections);
+  }
+})
+
+const addNewUrlCollection = () => {
+  if (newUrl.value != '') {
+    const urlCollection = {
+      id: crypto.randomUUID(),
+      url: newUrl.value,
+      title: newTitle.value
+    }
+    urlCollections.value.unshift(urlCollection)
+    localStorage.setItem('auo-url-collection-tool', JSON.stringify(urlCollections.value))
+    clearInput()
+  }
+  else
+    alert('請填寫網址')
+
+}
+
+const clearInput = () => {
+  newTitle = ''
+  newUrl = ''
+}
+
+const deleteAll = () => {
+  if (confirm(`確定清空全部`)) {
+    urlCollections.value = []
+    setLocalStorage()
+  }
+}
+
+const deleteUrlCollection = (urlCollection) => {
+  if (confirm(`確定刪除：${formatUrlCollection(urlCollection)}`)) {
+    urlCollections.value = urlCollections.value.filter(e => e.id !== urlCollection.id)
+    setLocalStorage()
+  }
+}
+
+const setLocalStorage = () => {
+  localStorage.setItem('auo-url-collection-tool', urlCollections.value)
+}
+
+const formatUrlCollection = (urlCollection) => {
+  return `${urlCollection.titel != '' ? `[${urlCollection.title}]` : ''}(${urlCollection.url})`
+}
 </script>
 
 <template>
-  <h1>Hello world!!!!</h1>
+  <div>
+    <h1>收藏網址⼩⼯具</h1>
+    <input @keyup.enter="addNewUrlCollection" v-model.trim="newTitle" type="text" class="input" placeholder="標題" />
+    <input @keyup.enter="addNewUrlCollection" v-model.trim="newUrl" type="text" class="input" placeholder="網址" />
+    <button @click="addNewUrlCollection">新增收藏</button>
+    <button @click="deleteAll">清除全部</button>
+    <ul v-for="urlCollection in urlCollections">
+      <li>{{ formatUrlCollection(urlCollection) }}</li><button @click="deleteUrlCollection(urlCollection)">刪除收藏</button>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
